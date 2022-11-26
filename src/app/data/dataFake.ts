9e394a1fee87ba2241a38e9be7565c3f03a4dea5
josenildolins <josenildo.lins@gmail.com>
2022-11-26 00:50:46 -0300

    Content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,12 +1,54 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "saiu novo filme",
-    "description": "ola",
-    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqFTnUZhP5BoG2JeY2fYo2ypFduthqT_VQNQ&usqp=CAU"
+    "title": "Estréia dia 22 de Dezembro de 2022",
+    "description": " Disponível na Warnner Bros",
+    "photoCover": "https://s2.glbimg.com/8dd4CjGZDaVi7kuWQkjBZ9SS8UA=/512x320/smart/e.glbimg.com/og/ed/f/original/2019/01/17/bb465df84ce53e566dddd64c69b2ff73-754x394.jpg"
   },
   {
     "id":"2",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"3",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"4",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"5",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"6",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"7",
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://sm.ign.com/ign_br/news/m/marvels-av/marvels-avengers-15-unannounced-characters-reportedly-found_ypfr.jpg"
+  },
+  {
+    "id":"8",
     "photo": "aaa",
     "title": "saiu novo filme",
     "description": "ola",
